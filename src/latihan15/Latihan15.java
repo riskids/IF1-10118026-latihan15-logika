a88e7102e7eaccc199864fb7b77e1cb618cd0a29
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:47:47 +0700

    Add files via upload

---
+++ src/latihan15/Latihan15.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan15;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar BIT
+ * 
+ */
+public class Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " +(a && b));
+        System.out.println("a || b = " +(a || b));
+        System.out.println("!a = " +(!a));
+        System.out.println("!(a && b) = " +(!(a && b)));
+    }
+    
+}
